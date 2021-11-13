f674aaf9b1f7effe4b2363d36a3d1af4d7c99baf
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2021-11-13 20:15:42 -0300

    including initial implementation of CarController

--- car-inventory-service/service/src/main/java/io/codex/service/controller/CarController.java
+++ car-inventory-service/service/src/main/java/io/codex/service/controller/CarController.java
@@ -1,7 +1,11 @@
 package io.codex.service.controller;
 
+import io.codex.api.car.request.CarRequest;
 import io.codex.api.car.resource.CarResource;
+import io.codex.api.car.response.CarResponse;
 import io.codex.model.service.CarService;
+import io.codex.service.transformer.CarTransformer;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +15,16 @@ public class CarController implements CarResource {
 
     public CarController(CarService carService) {
         this.carService = carService;
+    }
+
+    @Override
+    public CarResponse addCar(CarRequest carRequest) {
+        final var savedCar = carService.saveCar(CarTransformer.toEntity(carRequest));
+        return CarTransformer.toResponse(savedCar);
+    }
+
+    @Override
+    public Map<String, Object> findCars(Integer page, Integer size, String sort) {
+        return carService.findAll(page, size, sort);
     }
 }
