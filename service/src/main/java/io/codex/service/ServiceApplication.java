d793aae9d46e16a3adfa7c2efbcd839f41c1f6c0
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2021-11-13 07:02:44 -0300

    including modules into the base package scan and adding the controller layer

---
+++ service/src/main/java/io/codex/service/ServiceApplication.java
@@ -0,0 +1,16 @@
+package io.codex.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+    "io.codex.service",
+    "io.codex.model",
+    "io.codex.modeldb",
+})
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
