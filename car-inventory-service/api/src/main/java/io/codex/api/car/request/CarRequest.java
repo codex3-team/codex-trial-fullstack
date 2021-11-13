56613bdc7144d35a0e4fd1d87f9e9048fb7d2d61
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2021-11-13 20:12:33 -0300

    initial implmentation of API definition

--- car-inventory-service/api/src/main/java/io/codex/api/car/request/CarRequest.java
+++ car-inventory-service/api/src/main/java/io/codex/api/car/request/CarRequest.java
@@ -2,4 +2,29 @@ package io.codex.api.car.request;
 
 public class CarRequest {
 
+    private String make;
+    private String model;
+    private String year;
+
+    protected CarRequest() {
+    }
+
+    public CarRequest(String make, String model, String year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
 }
--- car-inventory-service/api/src/main/java/io/codex/api/car/response/CarResponse.java
+++ car-inventory-service/api/src/main/java/io/codex/api/car/response/CarResponse.java
@@ -1,5 +1,37 @@
 package io.codex.api.car.response;
 
+import java.util.UUID;
+
 public class CarResponse {
 
+    private UUID id;
+    private String make;
+    private String model;
+    private String year;
+
+    protected CarResponse() {
+    }
+
+    public CarResponse(UUID id, String make, String model, String year) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
 }
